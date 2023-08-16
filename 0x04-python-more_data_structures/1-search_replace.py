fd7fae2c0e03f663bec51032a66bef51e0138469
leone-nyaga <leonenyaga@gmail.com>
2023-08-16 18:41:19 +0000

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 1-search_replace.py
+
+def search_replace(my_list, search, replace):
+    result_list = []
+
+    for item in my_list:
+        if item == search:
+            result_list.append(replace)
+        else:
+            result_list.append(item)
+
+    return result_list
