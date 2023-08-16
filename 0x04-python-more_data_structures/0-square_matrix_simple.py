9ffe3714258225e94f0029f4708ed82abb36c543
leone-nyaga <leonenyaga@gmail.com>
2023-08-16 12:13:47 -0700

    Run Python file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,10 +3,7 @@
 
 def square_matrix_simple(matrix=[]):
     squared_matrix = []
-    
     for row in matrix:
         squared_row = [x ** 2 for x in row]
         squared_matrix.append(squared_row)
-    
     return squared_matrix
-
