c645cf7f4ea4f5396fad5e4caa2a1d2ab1050e78
leone-nyaga <leonenyaga@gmail.com>
2023-08-16 18:34:47 +0000

    Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-square_matrix_simple.py
+
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+    
+    for row in matrix:
+        squared_row = [x ** 2 for x in row]
+        squared_matrix.append(squared_row)
+    
+    return squared_matrix
+
