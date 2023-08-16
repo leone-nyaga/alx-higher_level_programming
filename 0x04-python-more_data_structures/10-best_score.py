94038e97fa7fbb048a097b68ae66cd5ce570ef13
leone-nyaga <leonenyaga@gmail.com>
2023-08-16 13:16:02 -0700

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 10-best_score.py
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max_key = None
+    max_value = float('-inf')  # Start with negative infinity
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+    return max_key
