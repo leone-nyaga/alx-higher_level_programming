7b492db83fbb5c4b65002fa4eb1c43a8d9799d2d
leone-nyaga <125727613+leone-nyaga@users.noreply.github.com>
2023-10-19 18:38:03 +0300

    sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,10 @@
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows.
 
-SELECT tv_shows.title
-FROM tv_shows
-INNER JOIN tv_show_genres
+Select tv_shows.title
+From tv_shows
+Inner JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title;
+Inner JOIN tv_genres
+On tv_show_genres.genre_id = tv_genres.id
+WherE tv_genres.name = "Comedy"
+ORder BY tv_shows.title;
