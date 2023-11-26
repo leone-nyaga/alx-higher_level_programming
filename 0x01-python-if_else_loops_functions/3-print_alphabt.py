0d3aefe0eddac2a8248c5b89f57b58829f3433af
leone-nyaga <leonenyaga@gmail.com>
2023-11-26 06:33:50 -0800

    correction

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-    for letter in range(ord('a'), ord('z') + 1):
-        if chr(letter) not in ['q', 'e']:
-            print("{:c}".format(letter), end="")
-
+for i in range(97, 123):
+    if chr(i) not in ("q", "e"):
+        print("{}".format(chr(i)), end='')
