221ee2e809c056d34f0014bef63521a27930b247
leone-nyaga <leonenyaga@gmail.com>
2024-02-20 13:38:44 -0800

    JS

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+let id = parseInt(process.argv[2], 10);
+let character = [];
+
+request(url, function (err, response, body) {
+	if (err === null) {
+		const filmdata = JSON.parse(body);
+		const results = filmData.results;
+
+		if (id < 4) {
+			id += 3;
+		} else {
+			id -= 3;
+		}
+
+		for (let i = 0; i < results.length; i++) {
+			if (results[i].episode_id === id) {
+				characters = result[i].characters;
+				break;
+			}
+		}
+
+		for (let j = 0; j < characters.length; j++) {
+			request(characters[j], function (err, response, body) {
+				if (err === null) {
+					const characterData = JSON.parse(body);
+					console.log(characterData.name);
+				}
+			});
+		}
+	}
+});
