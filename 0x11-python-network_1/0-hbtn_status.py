ff48b07c0c3693827949a399a81493c0952a8b1e
leone-nyaga <leonenyaga@gmail.com>
2024-04-22 03:11:06 -0700

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,3 +10,4 @@ with request.urlopen(url) as response:
 print("Body response:")
 print("\t- type: {}".format(type(body)))
 print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body))
