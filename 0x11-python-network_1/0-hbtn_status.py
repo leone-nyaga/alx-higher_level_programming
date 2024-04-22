49a83a7a3c998a7e46d33cb172cc420f02380069
leone-nyaga <leonenyaga@gmail.com>
2024-04-22 03:06:45 -0700

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ url = 'https://alx-intranet.hbtn.io/status'
 with request.urlopen(url) as response:
     body = response.read().decode('utf-8')
 
-print("- Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
