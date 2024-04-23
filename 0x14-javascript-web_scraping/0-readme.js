8d3271e47cc2ed5727343c57f2fc702d5974ce37
leone-nyaga <leonenyaga@gmail.com>
2024-04-23 06:01:29 -0700

    js web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const filepath = process.argv[2];
-fs.readFile(filepath, 'utf8', (err, data) {
+fs.readFile(filepath, 'utf8', (err, data) => {
 	if (err) {
 		console.log(err);
 	} else {
